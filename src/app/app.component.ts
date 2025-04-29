333136c0ba48069edb023cd2461a9b44dc3bfd49
kbenjemmaa <khalil.ben.jammaa@gmail.com>
2025-04-29 14:21:26 +0100

    FIX SCRIPTS

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
   title = 'SISES-APP';
+  constructor(private renderer : Renderer2) { }
+  ngAfterViewInit(): void {
+   const script = this.renderer.createElement('script');
+   script.src = 'assets/js/main.js';
+   script.type = 'text/javascript';
+   this.renderer.appendChild(document.body, script);
+  }
 }
