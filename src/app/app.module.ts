e9f2b3b5424dac96cef87e87a3fb7df68d722be9
kbenjemmaa <khalil.ben.jammaa@gmail.com>
2025-05-05 11:22:09 +0100

    ADD FEATURES

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,21 @@ import { AppComponent } from './app.component';
 import { HomePageComponent } from './home-page/home-page.component';
 import { WorldMapComponent } from './components/world-map/world-map.component';
 import { SafeHtmlPipe } from './pipes/safe-html.pipe';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
+import { StatsComponent } from './stats/stats.component';
+import { TestimonialComponent } from './testimonial/testimonial.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomePageComponent,
     WorldMapComponent,
-    SafeHtmlPipe
+    SafeHtmlPipe,
+    FooterComponent,
+    HeaderComponent,
+    StatsComponent,
+    TestimonialComponent
   ],
   imports: [
     BrowserModule,
