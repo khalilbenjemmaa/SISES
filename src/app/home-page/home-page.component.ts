4dbdaf2b09a769cdbb57c58aa5f4c1f43a6149d9
Khalil BEN JEMMAA <kbenjemmaa@production.priv>
2025-05-13 00:54:09 +0100

    [FIX] TYPOS

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -1,4 +1,5 @@
 import { AfterViewInit, Component, Renderer2 } from '@angular/core';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
 @Component({
   selector: 'app-home-page',
@@ -7,5 +8,15 @@ import { AfterViewInit, Component, Renderer2 } from '@angular/core';
 })
 export class HomePageComponent  {
 
+  modalImage: string = ''; // This will store the image URL to display in the modal
 
+  constructor() {}
+
+  // Method to open the modal and set the image source
+  openModal(imageSrc: string) {
+    this.modalImage = imageSrc;
+    // Bootstrap modal show method
+    const modalElement = document.getElementById('certificationModal');
+    
+  }
 }
